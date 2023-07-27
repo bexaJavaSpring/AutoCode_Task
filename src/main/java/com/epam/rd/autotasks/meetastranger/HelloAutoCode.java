2132fd6260807490414e4dfbc92d2b482b7f9f7d
Behruz <bexruzizzatullayev@gmail.com>
2023-07-27 14:03:58 +0500

    Solved

---
+++ src/main/java/com/epam/rd/autotasks/meetastranger/HelloAutoCode.java
@@ -0,0 +1,9 @@
+package com.epam.rd.autotasks.meetastranger;
+
+public class HelloAutoCode {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello, AutoCode!");
+    }
+}
